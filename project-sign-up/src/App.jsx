97ee31531e510b500fa3b32c00007bfae4270ca1
Donna Yau <47676379+dyau172@users.noreply.github.com>
2022-06-22 12:30:40 +0100

    added components

--- project-sign-up/src/App.jsx
+++ project-sign-up/src/App.jsx
@@ -1,10 +1,20 @@
-
+import "./app.scss";
+import SignupForm from "./components/SignupForm";
+import ImageSidebar from './components/ImageSidebar';
 
 function App() {
   return (
     <div className="app">
-    
+      <div className="left">
+        <ImageSidebar/>
+      </div>
+      <div className="right">
+        <SignupForm/>
+      </div>
+
     </div>
+
+
   );
 }
 
