20888b12386ae5eac6d4f150bc66f4d970bc0f3d
Donna Yau <47676379+dyau172@users.noreply.github.com>
2022-07-11 17:53:58 +0100

    import components

--- 2-admin-dashboard/src/App.jsx
+++ 2-admin-dashboard/src/App.jsx
@@ -1,8 +1,20 @@
 import './app.scss';
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import Dashboard from './components/Dashboard';
+import Annoucements from './components/Announcements';
+
 
 function App() {
   return (
     <div className="app">
+   <NavBar/>
+   <Header/>
+   <div className="section">
+    <Dashboard/>
+    <Annoucements/>
+   </div>
+      
 
     </div>
   );
