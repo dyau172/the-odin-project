43b0fdad7e6348bcce4923917390b0180f359964
Donna Yau <47676379+dyau172@users.noreply.github.com>
2022-08-17 10:21:38 +0100

    created header

--- 2-admin-dashboard/src/components/Header.jsx
+++ 2-admin-dashboard/src/components/Header.jsx
@@ -1,7 +1,45 @@
 import React from './header.scss'
+import Icon from '@mdi/react'
+import { mdiBellRingOutline, mdiMagnify } from '@mdi/js';
+import avatar from '../images/pngaaa.com-1582345.png'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="header">
+      <div className="top">
+
+        <form>
+          <Icon className='icon-mag'
+            path={mdiMagnify}
+            size={1} />
+
+          <input className='searchInput'
+          />
+
+        </form>
+
+
+        <div className="user">
+          <Icon className='icon'
+            path={mdiBellRingOutline}
+            size={1} />
+          <img className='sm-avatar' src={avatar} alt="" />
+          <p>Morgan Oakley</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <img className='avatar' src={avatar} alt="" />
+        <div className="details">
+          <p>Hi there,</p>
+          <h5>Morgan Oakley (@morgan)</h5>
+        </div>
+        <div className="links">
+          <button>New</button>
+          <button>Upload</button>
+          <button>Share</button>
+        </div>
+      </div>
+    </div>
+
   )
 }
