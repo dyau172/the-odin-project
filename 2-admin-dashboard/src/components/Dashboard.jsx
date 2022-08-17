9bbb50cb60a46ccbb7463137b17f01d6700844ff
Donna Yau <47676379+dyau172@users.noreply.github.com>
2022-08-17 10:21:27 +0100

    created dashboard

--- 2-admin-dashboard/src/components/Dashboard.jsx
+++ 2-admin-dashboard/src/components/Dashboard.jsx
@@ -2,6 +2,39 @@ import React from './dashboard.scss'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className="dashboard">
+      <div className="container">
+        <h2>Projects</h2>
+        <div className="card-column">
+          <div className="card">
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+          </div>
+          <div className="card">
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+          </div>
+          <div className="card">
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+          </div>
+        </div>
+        <div className="card-column">
+          <div className="card">
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+          </div>
+          <div className="card">
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+          </div>
+          <div className="card">
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
