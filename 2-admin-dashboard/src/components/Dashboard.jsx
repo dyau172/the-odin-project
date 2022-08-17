d968ab8ebd1bfedb01195f093a92cce75c650cda
Donna Yau <47676379+dyau172@users.noreply.github.com>
2022-08-17 11:08:01 +0100

    added interaction icons

--- 2-admin-dashboard/src/components/Dashboard.jsx
+++ 2-admin-dashboard/src/components/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React from './dashboard.scss'
+import Icon from '@mdi/react'
+import { mdiStarPlusOutline, mdiEyePlusOutline, mdiSourceFork } from '@mdi/js';
 
 export default function Dashboard() {
   return (
@@ -9,28 +11,94 @@ export default function Dashboard() {
           <div className="card">
             <h3>Card title</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+            <div className="interactions">
+              <Icon className='icon'
+                path={mdiStarPlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiEyePlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiSourceFork}
+                size={1} />
+            </div>
           </div>
           <div className="card">
             <h3>Card title</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+            <div className="interactions">
+              <Icon className='icon'
+                path={mdiStarPlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiEyePlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiSourceFork}
+                size={1} />
+            </div>
           </div>
           <div className="card">
             <h3>Card title</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+            <div className="interactions">
+              <Icon className='icon'
+                path={mdiStarPlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiEyePlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiSourceFork}
+                size={1} />
+            </div>
           </div>
         </div>
         <div className="card-column">
           <div className="card">
             <h3>Card title</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+            <div className="interactions">
+              <Icon className='icon'
+                path={mdiStarPlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiEyePlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiSourceFork}
+                size={1} />
+            </div>
           </div>
           <div className="card">
             <h3>Card title</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+            <div className="interactions">
+              <Icon className='icon'
+                path={mdiStarPlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiEyePlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiSourceFork}
+                size={1} />
+            </div>
           </div>
           <div className="card">
             <h3>Card title</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestias ratione amet alias exercitationem quasi et totam nostrum mollitia aliquam.</p>
+            <div className="interactions">
+              <Icon className='icon'
+                path={mdiStarPlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiEyePlusOutline}
+                size={1} />
+              <Icon className='icon'
+                path={mdiSourceFork}
+                size={1} />
+            </div>
           </div>
         </div>
 
